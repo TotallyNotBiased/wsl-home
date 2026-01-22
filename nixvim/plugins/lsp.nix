9988daa1b6115821e6d5d90297469a37c27d83aa
TotallyNotBiased <highfunctioningpsychopath@gmail.com>
2026-01-22 16:11:11 +1100

    removing cargo and rustc from nixvim

--- nixvim/plugins/lsp.nix
+++ nixvim/plugins/lsp.nix
@@ -4,8 +4,8 @@
     servers = {
       rust_analyzer = {
 	enable = true;
-	installCargo = true;
-	installRustc = true;
+	installCargo = false;
+	installRustc = false;
 
 	settings = {
 	  inlayHints = {
